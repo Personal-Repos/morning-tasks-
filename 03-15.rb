fd72b0f72888ca95aedc6270c0e6a85c9e123b24
pakinamelbanna <pakinam_banna@hotmail.com>
2017-03-15 14:42:54 +0200

    solve problem 2

---
+++ 03-15.rb
@@ -0,0 +1,13 @@
+#! /usr/bin/env ruby
+
+array = [1, 0, 1, 1, 0, 1]
+sum = 0
+i = 0
+power_counter = array.length - 1
+
+while i < array.length
+  sum += array[i] * (2**power_counter)
+  power_counter -= 1
+  i += 1
+end
+puts "The decimal value for the given array of binary bits #{array} is #{sum}"
