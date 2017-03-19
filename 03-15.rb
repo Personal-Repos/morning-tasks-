9cf7d2d585d1b22a2c01525e772af2c17798c2c5
sondossamii <sondos379@gmail.com>
2017-03-19 13:02:14 +0200

    Merge Exercise 2 Solution with the others solutions

--- 03-15.rb
+++ 03-15.rb
@@ -33,6 +33,19 @@
 #     so far is #{sum}"
 # end
 
+# Solve Example 2
+# array = [1, 0, 1, 1, 0, 1]
+# sum = 0
+# i = 0
+# power_counter = array.length - 1
+#
+# while i < array.length
+#   sum += array[i] * (2**power_counter)
+#   power_counter -= 1
+#   i += 1
+# end
+# puts "The decimal value for the given array of binary bits #{array} is #{sum}"
+ 
 # Exercise 3
 array = [34, 60, 71, 34, 80, 100, 7]
 
